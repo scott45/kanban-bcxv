c94c6baccca6c129189792abe95079233df147a4
Scott B5 <busingescott@gmail.com>
2017-02-14 10:32:08 +0300

    Created ToDo class and i initiated it with required parameters

--- app/to_do.py
+++ app/to_do.py
@@ -1,1 +1,17 @@
 __author__ = 'Scott Businge'
+
+import sqlite3 as db
+import datetime as datetime
+import time as guage
+
+
+class ToDo(object):
+
+    def __init__(self):
+
+        self.datetime = datetime
+        self.time = guage
+        self.data = 'mydatabase.db'
+
+
+
