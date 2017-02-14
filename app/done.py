5d7b44fcb62f91516ebcc3de34f78e33d3a1821e
Scott B5 <busingescott@gmail.com>
2017-02-14 16:29:54 +0300

    Change database name to debug not reached database error

--- app/done.py
+++ app/done.py
@@ -8,11 +8,11 @@ import time as guage
 
 # class done
 class Done(to_do):
-    # done function for done that is to handle all tasks that are done.
+    # done function for done that is to handle all tasks that are done.3
     def done(self, task_id, task_off):
         if task_off:
             end_time = datetime.datetime.fromtimestamp(guage.time()).strftime('%Y-%m-%d %H:%M:%S')
-            sort = 'UPDATE todo SET task_status = "done",time_stop ="{}" WHERE task_id = {}'.format(end_time,
+            sort = 'UPDATE mydatabase SET task_status = "done",time_stop ="{}" WHERE task_id = {}'.format(end_time,
                                                                                                        task_id)
             self.db.execute(sort)
             self.db.commit()
