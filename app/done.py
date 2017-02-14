3155970c98fea7ea4bb21eb442c90ecc54cc7e6c
Scott B5 <busingescott@gmail.com>
2017-02-14 12:09:24 +0300

    Defined the done function that will label tasks as done

---
+++ app/done.py
@@ -0,0 +1,19 @@
+__author__ = 'Scott Businge'
+
+# import statements
+from app import to_do
+import datetime
+import time as guage
+
+
+# class done
+class Done(to_do):
+    # done function for done that is to handle all tasks that are done.
+    def done(self, task_id, task_off):
+        if task_off:
+            end_time = datetime.datetime.fromtimestamp(guage.time()).strftime('%Y-%m-%d %H:%M:%S')
+            sort = 'UPDATE todo SET task_status = "done",time_stop ="{}" WHERE task_id = {}'.format(end_time,
+                                                                                                       task_id)
+            self.db.execute(sort)
+            self.db.commit()
+            print("Task accomplished")
