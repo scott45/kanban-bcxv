7d70e6a6581c06aa1c542e4d8295c018e6fde740
Scott B5 <busingescott@gmail.com>
2017-02-14 10:46:03 +0300

    Described the tasks database table within ToDo class

--- app/to_do.py
+++ app/to_do.py
@@ -13,5 +13,21 @@ class ToDo(object):
         self.time = guage
         self.data = 'mydatabase.db'
 
+        self.db = db.connect(self.data)
+        self.cursor = self.db.cursor()
+
+        try:
+            self.db.execute('''create table to_do
+                             (task_id INTEGER PRIMARY KEY,
+                             task_name TEXT NOT NULL,
+                             task_detail TEXT NOT NULL,
+                             task_label TEXT ,
+                             task_on DATETIME,
+                             time_off DATETIME,
+                             time_taken DATETIME)''')
+
+        except db.OperationalError:
+            pass
+
 
 
