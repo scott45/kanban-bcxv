d7cdce00fbbe0f8afd7aa6dbb0a6ab5191bfb0ac
Scott B5 <busingescott@gmail.com>
2017-02-14 18:09:25 +0300

    Restructure the class name to ease import

--- app/doing.py
+++ app/doing.py
@@ -1,19 +1,19 @@
 __author__ = 'Scott Businge'
 
 # import statements
-from app import to_do
+from app.to_do import ToDo
 import datetime
 import time as guage
 
 
 # class doing
-class Doing(to_do):
-
+class Doing(ToDo):
     # current function for doing that is to handle all tasks being done now.
     def doing(self, task_id, task_on):
         if task_on:
             begin_time = datetime.datetime.fromtimestamp(guage.time()).strftime('%Y-%m-%d %H:%M:%S')
-            move = 'UPDATE mydatabase SET task_label = "doing",task_on ="{}" WHERE task_id = {}'.format(begin_time, task_id)
+            move = 'UPDATE mydatabase SET task_label = "doing",task_on ="{}" WHERE task_id = {}'.format(begin_time,
+                                                                                                        task_id)
             self.db.execute(move)
             self.db.commit()
             print("Moved to Doing")
