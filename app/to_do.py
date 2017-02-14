e58b724737ac27e076dc821a8f7f9ce1159f0251
Scott B5 <busingescott@gmail.com>
2017-02-14 16:26:20 +0300

    Renamed database to eliminate inbuilt shadows

--- app/to_do.py
+++ app/to_do.py
@@ -19,7 +19,7 @@ class ToDo(object):
         self.cursor = self.db.cursor()
 
         try:
-            self.db.execute('''CREATE TABLE to_do
+            self.db.execute('''CREATE TABLE mydatabase
                              (task_id INTEGER PRIMARY KEY,
                              task_name TEXT NOT NULL,
                              task_detail TEXT NOT NULL,
@@ -34,7 +34,7 @@ class ToDo(object):
             # function to perform task_add
 
     def to_do(self, name, detail):
-        self.db.execute('INSERT INTO to_do(task_name, task_detail, task_label) VALUES (?,?,?)', (name,
+        self.db.execute('INSERT INTO mydatabase(task_name, task_detail, task_label) VALUES (?,?,?)', (name,
                                                                                                  detail, 'todo'))
         self.db.commit()
         print("Saved to-do input")
